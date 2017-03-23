667c378349e44d09247cdac54983fe0e6a7900b2
Justin Maguire <maguirejustin@hotmail.com>
2017-03-23 16:14:49 -0400

    Created Pledges, model, controller and view partials

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root 'projects#index'
   resources :projects do
     resources :rewards, only: [ :new, :create, :edit, :update, :destroy ]
+    resources :pledges
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
